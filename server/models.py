4bac17b0710d1ab5d678f91ae8ef10272fea512f
jojo <georgebadia2019@gmail.com>
2024-10-11 19:48:17 +0300

    creates appearance model

--- server/models.py
+++ server/models.py
@@ -36,3 +36,34 @@ class Guest(db.Model):
             "occupation": self.occupation
         }
 
+# Appearance model
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+   #Add relationships
+    episode = db.relationship('Episode', back_populates='appearances')
+    guest = db.relationship('Guest', back_populates='appearances')
+   #add validation
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if not 1 <= rating <= 5:
+            raise ValueError("Rating must be between 1 and 5")
+        return rating
+   #convert the Appearance object to a dictionary
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "episode_id": self.episode_id,
+            "guest_id": self.guest_id,
+            "guest": self.guest.to_dict(),
+            "episode": {
+                "id": self.episode.id,
+                "date": self.episode.date,
+                "number": self.episode.number
+            }
+        }
